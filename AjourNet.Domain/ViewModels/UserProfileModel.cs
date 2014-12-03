429007241a8b2c446162d9d01db7d7ec80b4ce50
Anna Storozhenko <anna.storozhenko@ericpol.com>
2014-12-03 18:14:51 +0200

    Create UserProfile() method and view is added

--- AjourNet.Domain/ViewModels/UserProfileModel.cs
+++ AjourNet.Domain/ViewModels/UserProfileModel.cs
@@ -11,28 +11,36 @@ namespace AjourNet.Domain.ViewModels
 {
     public class UserProfileModel
     {
+        public int UserProfileID;
         [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
-      
-        [Required]
+
+        [Required(ErrorMessage = "First Name is required")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
-        
-        [Required]
+
+        [Required(ErrorMessage = "Last Name is required")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
         
         public  AjourNetUser AjourNetUser { get; set; }
-
+        public UserProfileModel()
+        {
+            FirstName = FirstName;
+            LastName = LastName;
+            AjourNetUser = new AjourNetUser();
+            UserName = AjourNetUser.UserName;
+        }
         public UserProfileModel(UserProfile user)
         {
+            UserProfileID = user.UserProfileID; 
             FirstName = user.FirstName;
             LastName = user.LastName;
             AjourNetUser = user.AjourNetUser;
             UserName = user.UserName;
 
         }
-
+        
     }
 }
--- AjourNet/Controllers/UserProfileController.cs
+++ AjourNet/Controllers/UserProfileController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AjourNet.Domain.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,34 @@ namespace AjourNet.Controllers
 {
     public class UserProfileController : Controller
     {
-        // GET: UserProfile
-        public ActionResult GetUserProfile()
+        // GET: CreateUserProfile
+        public ActionResult CreateUserProfile()
         {
             return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateUserProfile(UserProfileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("GetUserFeed");
+            }
+            else
+            {
+              //  ModelState.AddModelError("","Please, enter your credentials"); 
+                
+                return View(model);
+            }
+          
+        }
+
+        
+        public ActionResult GetUserFeed(UserProfileModel model)
+        {
+            return View("~\\Views\\Shared\\_Layout.cshtml");
         }
     }
 }
