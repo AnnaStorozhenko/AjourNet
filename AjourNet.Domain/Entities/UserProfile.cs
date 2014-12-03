fb10ddb60bb877add3c8b65ac95964ea428832d1
Anna Storozhenko <anna.storozhenko@ericpol.com>
2014-12-03 11:21:06 +0200

    AjourNetUser, UserProfile entityes are added

---
+++ AjourNet.Domain/Entities/UserProfile.cs
@@ -0,0 +1,28 @@
+﻿using AjourNet.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjourNet.Domain.Entities
+{
+    public class UserProfile
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        public virtual AjourNetUser AjourNetUser { get; set; }
+       
+        [Required]
+        public string UserName
+        {
+            get { return AjourNetUser.UserName; }
+        }
+
+
+    }
+}
