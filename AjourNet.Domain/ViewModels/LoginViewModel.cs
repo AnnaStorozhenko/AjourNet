1666eee5d344816c9181ab776250a87b79cc5dca
Bohdan Tereta <Bohdan.Tereta@ericpol.com>
2014-12-03 18:23:28 +0200

    Issue #10 UI mockup completed

---
+++ AjourNet.Domain/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjourNet.Domain.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage="That's not the right login")]
+        [Display(Name = "Login")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "That's not the right password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Keep me signed in")]
+        public bool RememberMe { get; set; }
+    }
+}
