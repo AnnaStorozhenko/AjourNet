72522918a97e1d7946dd9f5c7408b41f13217b33
Bohdan Tereta <Bohdan.Tereta@ericpol.com>
2014-12-04 17:53:39 +0200

    Task #40 UI is completed

--- AjourNet/Controllers/SearchController.cs
+++ AjourNet/Controllers/SearchController.cs
@@ -10,22 +10,37 @@ namespace AjourNet.Controllers
     public class SearchController : Controller
     {
 
-        public ActionResult SearchUser(string searchString)
+        public ActionResult SearchUser(string searchString="")
         {
-            if(searchString=="tebo")
+            UserSearchViewModel firstUser = new UserSearchViewModel
             {
-                UserSearchViewModel userFound =  new UserSearchViewModel
+                UserProfileID = 111,
+                FirstName = "Bohdan",
+                LastName = "Tereta",
+                Occupation = "Developer",
+                Location = "Lviv"
+            };
+
+            UserSearchViewModel secondUser = new UserSearchViewModel
+            {
+                UserProfileID = 111,
+                FirstName = "Anna",
+                LastName = "Storozhenko",
+                Occupation = "Developer",
+                Location = "Lviv"
+            };
+
+            IEnumerable<UserSearchViewModel> users = new List<UserSearchViewModel> { firstUser, secondUser };
+            List<UserSearchViewModel> usersFound = new List<UserSearchViewModel>();
+            foreach (UserSearchViewModel user in users)
+            {
+                if (user.FirstName.Contains(searchString))
                 {
-                    UserProfileID = 111,
-                    FirstName = "Bohdan",
-                    LastName = "Tereta", 
-                    Occupation = "Developer", 
-                    Location = "Lviv"
-                };
-                IEnumerable<UserSearchViewModel> usersFound = new List<UserSearchViewModel> { userFound }; 
+                    usersFound.Add(user);
+                }
             }
-            return PartialView();
-        } 
+            return PartialView(usersFound);
+        }
 
     }
 }
