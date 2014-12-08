8dd43d234c61ec41ed6b3afdf7109aac5f2d1d84
Anna Storozhenko <anna.storozhenko@ericpol.com>
2014-12-08 11:29:47 +0200

    DisplayDatysAgo helper is created

--- AjourNet/Controllers/PostController.cs
+++ AjourNet/Controllers/PostController.cs
@@ -21,16 +21,24 @@ namespace AjourNet.Controllers
 
         public ActionResult ViewMyPost(UserPostModel postModel)
         { 
-            UserPostModel post1 = new UserPostModel { PostID = 10, Time = DateTime.Now.ToLocalTimeAzure().Date.AddDays(-1), Text = "Hi All, my name is Nick" };
+            UserPostModel post1 = new UserPostModel { PostID = 10, Time = DateTime.Now.ToLocalTimeAzure().Date.AddDays(-2), Text = "Hi All, my name is Nick" };
             UserPostModel post2 = new UserPostModel { PostID = 11, Time = DateTime.Now.ToLocalTimeAzure().AddHours(-4), Text = "Hi All,I am Nick" };
-            List<UserPostModel> posts = new List<UserPostModel> { post1, post2 };
+            UserPostModel post3 = new UserPostModel { PostID = 12, Time = DateTime.Now.ToLocalTimeAzure().AddMonths(-5), Text = "Hi All,I am Nick" };
+
+            List<UserPostModel> posts = new List<UserPostModel> { post1, post2, post3 };
             if (postModel != null && postModel.Text != String.Empty && postModel.Text != null)
             {
                 posts.Add(postModel);
             }
-            return PartialView(posts);
+
+            return PartialView(SortingPostsByDateTime(posts));
         }
-        
+
+        public List<UserPostModel> SortingPostsByDateTime(List<UserPostModel> posts)
+        {
+            posts.Sort((x, y) => y.Time.CompareTo(x.Time));
+            return posts;
+        }
 
     }
 }
---
+++ AjourNet/Helpers/DisplayTimeAgo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjourNet.Helpers
+{
+    public static class DisplayTimeAgoHelper
+    {
+        public static MvcHtmlString TimeAgo(this DateTime date)
+        {
+            TimeSpan timeSince = DateTime.Now.Subtract(date);
+
+            if (timeSince.TotalMinutes < 1)
+                return new MvcHtmlString(String.Format("just now"));
+            if (timeSince.TotalMinutes < 2)
+                return new MvcHtmlString(String.Format("1 minute ago"));
+            if (timeSince.TotalMinutes < 60)
+                return new MvcHtmlString(String.Format("{0} minutes ago", timeSince.Minutes));
+            if (timeSince.TotalMinutes < 120)
+                return new MvcHtmlString(String.Format("1 hour ago"));
+            if (timeSince.TotalHours < 24)
+                return new MvcHtmlString(String.Format("{0} hours ago", timeSince.Hours));
+            if (timeSince.TotalDays == 1)
+                return new MvcHtmlString(String.Format("yesterday"));
+            if (timeSince.TotalDays < 7)
+                return new MvcHtmlString(String.Format("{0} days ago", timeSince.Days));
+            if (timeSince.TotalDays < 14)
+                return new MvcHtmlString(String.Format("last week"));
+
+            return new MvcHtmlString(String.Format("{0} {1:MMM} {2:yyyy}", date.Day.ToString(), date, date.Year.ToString()));
+
+        }
+
+    }
+}
